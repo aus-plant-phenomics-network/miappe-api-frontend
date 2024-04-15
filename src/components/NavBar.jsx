bff809f932157a1a6b4c68a065fd6fab3072d5b2
hoangson <lehoangsonsg@gmail.com>
2024-04-15 16:36:26 +0930

    feat: add nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -29,12 +29,12 @@ const NavBar = forwardRef(({ children, ...rest }, ref) => {
   return (
     <div {...rest} ref={ref}>
       <div
-        className="h-full overflow-auto bg-gray-200"
+        className="c-nav-bar-content h-full overflow-auto bg-gray-200"
         style={{ width: width }}
       >
         {children}
       </div>
-      <div className="h-full w-5 relative bg-gray-100 rounded-r-md">
+      <div className="c-nav-bar-button h-full w-5 relative bg-gray-100 rounded-r-md">
         {ExpandCollapseButton}
       </div>
     </div>
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,10 +1,36 @@
 import NavBar from "../components/NavBar";
 import { Outlet } from "react-router-dom";
+import navItems from "../assets/navItems.json";
+import ToggleList from "../components/ToggleList";
+
+function SideBar({ data, className, ...rest }) {
+  console.log(data)
+  const children = (
+    <div className={className} {...rest}>
+      {data ? (
+        Object.entries(data).map(([titleString, listData]) => (
+          <ToggleList
+            key={titleString}
+            titleString={titleString}
+            listData={listData}
+          />
+        ))
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+  return (
+    <NavBar id="nav-bar" className="h-screen w-fit flex">
+      {children}
+    </NavBar>
+  );
+}
 
 export default function Root() {
   return (
     <div className="flex">
-      <NavBar className="h-screen w-fit flex">Hello World</NavBar>
+      <SideBar className="h-full p-3 flex flex-col gap-y-3" data={navItems} />
       <div className="flex-grow-1 w-full">
         <Outlet />
       </div>
