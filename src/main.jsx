704db099a14ffb2fde9eade8586ce3b6f30460d7
hoangson <lehoangsonsg@gmail.com>
2024-04-22 15:35:11 +0930

    feat: refactor to use the UI package

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,25 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 import "./index.css";
-import Root from "./routes/root";
+import { Context } from "ailiyah-ui";
+import { theme } from "./assets/theme.ts";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root/>
+    element: <App />,
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Context.ThemeProvider value={theme}>
+      <RouterProvider router={router} />
+    </Context.ThemeProvider>
+    ,
   </React.StrictMode>
 );
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,36 +1,10 @@
-import NavBar from "../components/NavBar";
 import { Outlet } from "react-router-dom";
-import navItems from "../assets/navItems.json";
-import ToggleList from "../components/ToggleList";
-
-function SideBar({ data, className, ...rest }) {
-  console.log(data)
-  const children = (
-    <div className={className} {...rest}>
-      {data ? (
-        Object.entries(data).map(([titleString, listData]) => (
-          <ToggleList
-            key={titleString}
-            titleValue={titleString}
-            listValue={listData}
-          />
-        ))
-      ) : (
-        <></>
-      )}
-    </div>
-  );
-  return (
-    <NavBar id="nav-bar" className="h-screen w-fit flex">
-      {children}
-    </NavBar>
-  );
-}
+import { NavBar } from "../components/NavBar";
 
 export default function Root() {
   return (
     <div className="flex">
-      <SideBar className="h-full p-3 flex flex-col gap-y-3" data={navItems} />
+      <NavBar />
       <div className="flex-grow-1 w-full">
         <Outlet />
       </div>
