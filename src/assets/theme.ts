fbfd0e91813cde1e1686039749dd28bd2cd2520b
hoangson <lehoangsonsg@gmail.com>
2024-05-01 14:59:28 +0930

    feat: update form layout

--- src/assets/theme.ts
+++ src/assets/theme.ts
@@ -41,4 +41,41 @@ export const theme: PresetTheme = {
     ...defaultTheme.Icons,
     twWidth: "",
   },
+  FormRoot: {
+    twMaxWidth: "max-w-[600px]",
+    twFlex: "flex",
+    twFlexDirection: "flex-col",
+    twGap: "gap-y-4"
+  },
+  FormContent: {
+    twFlex: "flex",
+    twFlexDirection: "flex-col",
+    twGap: "gap-y-3",
+  },
+  FormLabelGroup: {
+    twFlex: "flex",
+    twGap: "gap-x-4",
+  },
+  FormLabel: {
+    twFontWeight: "font-bold",
+    twFlexGrow: "flex-1",
+  },
+  FormInput: {
+    twFlexGrow: "flex-1",
+    twBorderWidth: "border-2",
+    twBorderRadius: "rounded-md",
+    twPadding: "p-1",
+  },
+  FormComponent: {
+    twFlex: "flex",
+    twJustifyContent: "justify-end",
+  },
+  FormSubmitButton: {
+    twBackgroundColor:
+      "bg-neutral-200 hover:bg-neutral-300 dark:bg-neutral-700 dark:hover:bg-neutral-600",
+    twPadding: "py-2 px-3",
+    twBorderWidth: "border-2",
+    twBorderRadius: "rounded-lg",
+    twWidth: "w-[120px]",
+  },
 };
