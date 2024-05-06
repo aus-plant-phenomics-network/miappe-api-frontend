95bdb6f10098fa4982d2d9e5cbbfdf22133f2f38
hoangson <lehoangsonsg@gmail.com>
2024-05-06 11:38:39 +0930

    chore: export module components

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+export * from "./factory";
+export * from "./helpers";
+export * from "./navbar";
+export * from "./table";
+export * from "./form";
+
+export type * from "./types";
+export type * from "./navbar";
+export type * from "./table";
+export type * from "./form";
---
+++ src/routes/investigation/index.ts
@@ -0,0 +1,4 @@
+export * from "./form";
+export * from "./investigation";
+
+export type * from "./investigation.types";
