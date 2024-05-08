c22b2ccb53cb5eb097f7a745bb8d90ac416abd73
hoangson <lehoangsonsg@gmail.com>
2024-05-08 15:13:16 +0930

    feat: allow factory methods to redirect

--- src/handlers/factory.ts
+++ src/handlers/factory.ts
@@ -1,4 +1,4 @@
-import { Params } from "react-router-dom";
+import { Params, redirect } from "react-router-dom";
 import { string2Date } from "../components";
 import {
   AbstractDataType,
@@ -87,7 +87,14 @@ const createHandlers = <T extends AbstractDataType>(
 const createLoaderAction = <T extends AbstractDataType, Key extends string>(
   url: string,
   schema: AbstractSchemaType<T>,
-  idKey: Key
+  idKey: Key,
+  createRedirect?:
+    | string
+    | ((request: Request, params: Params<Key>, result: T) => string),
+  updateRedirect?:
+    | string
+    | ((request: Request, params: Params<Key>, result: T) => string),
+  deleteRedirect?: string | ((params: Params<Key>) => string)
 ) => {
   const handlers: HandlerType<T> = createHandlers(url);
   const parseData = async (request: Request): Promise<T> => {
@@ -111,11 +118,23 @@ const createLoaderAction = <T extends AbstractDataType, Key extends string>(
   const loaderById = async ({ params }: { params: Params<Key> }) => {
     return await handlers.getDataById(params[idKey]!);
   };
-  const actionCreate = async ({ request }: { request: Request }) => {
+  const actionCreate = async ({
+    request,
+    params,
+  }: {
+    request: Request;
+    params: Params<Key>;
+  }) => {
     console.log("Create action");
     const submitData = await parseData(request);
     let result = await handlers.createData(submitData);
     console.log("POST result", result);
+    if (createRedirect)
+      return redirect(
+        typeof createRedirect === "string"
+          ? createRedirect
+          : createRedirect(request, params, result)
+      );
     return result;
   };
   const actionUpdate = async ({
@@ -126,10 +145,24 @@ const createLoaderAction = <T extends AbstractDataType, Key extends string>(
     params: Params<Key>;
   }) => {
     const submitData = await parseData(request);
-    return await handlers.updateData(submitData, params[idKey]!);
+    const result = await handlers.updateData(submitData, params[idKey]!);
+    if (updateRedirect)
+      return redirect(
+        typeof updateRedirect === "string"
+          ? updateRedirect
+          : updateRedirect(request, params, result)
+      );
+    return result;
   };
   const actionDelete = async ({ params }: { params: Params<Key> }) => {
-    return await handlers.deleteData(params[idKey]!);
+    const result = await handlers.deleteData(params[idKey]!);
+    if (deleteRedirect)
+      return redirect(
+        typeof deleteRedirect === "string"
+          ? deleteRedirect
+          : deleteRedirect(params)
+      );
+    return result;
   };
   return {
     handlers: handlers,
