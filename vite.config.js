285d4077cefb92ccb82e12129068a667ade3904b
harryle <lehoangsonsg@gmail.com>
2024-05-08 19:08:27 +0930

    chore: update dependencies - add coverage

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
+    coverage: {
+      thresholds: {
+        lines: 70,
+        branches: 70,
+        functions: 70,
+        statements: 70
+      },
+      enabled: true,
+      provider: "istanbul",
+      reportOnFailure: true,
+      reporter: "html"
+    },
     global: true,
     environment: "jsdom",
     setupFiles: "./setupTest",
