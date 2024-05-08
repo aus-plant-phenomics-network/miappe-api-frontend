98a77d4663dc90f652c56fedd5329fc862de4b86
hoangson <lehoangsonsg@gmail.com>
2024-05-08 09:58:25 +0930

    feat: add utility methods for processing file names

--- src/components/helpers.test.ts
+++ src/components/helpers.test.ts
@@ -1,13 +1,26 @@
-import { capitalise } from "./helpers";
+import { capitalise, toSnakeCase } from "./helpers";
 import { describe, expect, test } from "vitest";
 
-const testFixture = [
+const capitaliseFixture = [
   ["firstName", "FirstName"],
   ["lastName", "LastName"],
+  ["first name", "FirstName"],
+  ["last name", "LastName"],
 ];
 
-describe.each(testFixture)("given %s", (inputValue, expValue) => {
-  test(`returns ${expValue}`, () => {
+describe.each(capitaliseFixture)("given %s", (inputValue, expValue) => {
+  test(`capitalise returns ${expValue}`, () => {
     expect(capitalise(inputValue)).toBe(expValue);
+  });
+});
+
+const snakeCaseFixture = [
+  ["first name", "first_name"],
+  ["public release date", "public_release_date"],
+];
+
+describe.each(snakeCaseFixture)("given %s", (inputValue, expValue) => {
+  test(`toSnakeCase returns ${expValue}`, () => {
+    expect(toSnakeCase(inputValue)).toBe(expValue);
   });
 });
--- src/components/helpers.ts
+++ src/components/helpers.ts
@@ -1,5 +1,19 @@
-const capitalise = (text: string) => {
-  return text[0].toUpperCase() + text.slice(1);
+const toSnakeCase = (text: string) => {
+  let processedText = text.trim();
+  processedText = processedText.replace(/ /g, "_");
+  return processedText;
 };
 
-export { capitalise };
+const capitalise = (text: string) => {
+  let processedText = toSnakeCase(text);
+  const snakeToCamel = (s: string) =>
+    s.replace(/(_\w)/g, (k) => k[1].toUpperCase());
+  processedText = snakeToCamel(processedText);
+  return processedText[0].toUpperCase() + processedText.slice(1);
+};
+
+const string2Date = (text?: string) => {
+  return text ? new Date(text) : null;
+};
+
+export { capitalise, toSnakeCase, string2Date };
