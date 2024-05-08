22c9b455920d81e75f256464e59415650690a8f1
hoangson <lehoangsonsg@gmail.com>
2024-05-08 12:15:11 +0930

    chore: refactor some types to handlers factory

--- src/components/types.ts
+++ src/components/types.ts
@@ -1,19 +1,9 @@
 import React = require("react");
 import { TailwindComponentProps } from "@ailiyah-ui/factory";
 
-interface InputType {
-  type: React.HTMLInputTypeAttribute;
-  required: boolean;
-  placeholder?: string;
-}
-
-interface InputArrayType {
-  [name: string]: InputType;
-}
-
 interface NameRequiredInputProps
   extends Omit<TailwindComponentProps<"input">, "name"> {
   name: string;
 }
 
-export type { InputType, InputArrayType, NameRequiredInputProps };
+export type { NameRequiredInputProps };
