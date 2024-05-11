8dd4612f5a2c0851b8ad47f1a447a15daea5da4d
harryle <lehoangsonsg@gmail.com>
2024-05-11 18:35:22 +0930

    feat: add study page

---
+++ src/routes/study/study.tsx
@@ -0,0 +1,13 @@
+import { StudyType } from "./study.types";
+import { StudySchema } from "./study.types";
+import { createPages } from "../../components/factory/page";
+
+let schema: StudySchema = (await import("./data.json")).default;
+
+const [StudyList, StudyCreate, StudyUpdate] = createPages<StudyType>(
+  "study",
+  schema,
+  ["title", "description", "startDate"]
+);
+
+export { StudyCreate, StudyList, StudyUpdate };
