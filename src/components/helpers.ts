fbc9b16383d75aac5b56c650228ff63efb07a86b
hoangson <lehoangsonsg@gmail.com>
2024-05-12 22:51:47 +0930

    feat: update basic types and add utility methods

--- src/components/helpers.ts
+++ src/components/helpers.ts
@@ -1,31 +1,72 @@
-const toSnakeCase = (text: string) => {
-  return text.replace(/ /g, "_");
-};
+import { SchemaElementType } from "./types";
 
-const capitalise = (text: string) => {
-  let processedText = text.trim();
-  if (processedText.length == 0) return "";
-  if (processedText.length == 1) return text[0]?.toUpperCase();
-
-  processedText = toSnakeCase(processedText);
-  const snakeToCamel = (s: string) =>
-    s.replace(/(_\w)/g, k => k[1]!.toUpperCase());
-  processedText = snakeToCamel(processedText);
-  return processedText[0]!.toUpperCase() + processedText.slice(1);
-};
-
-const removeId = (text: string) => {
-  return text.endsWith("Id") ? text.substring(0, text.length - 2) : text;
-};
-
-const string2Date = (text?: string | null) => {
-  if (text) {
-    const parsedDate = new Date(text);
-    if (!isNaN(parsedDate.getDate())) {
-      return parsedDate;
+/**
+ * Get defaultValue prop's value for an input or select element,
+ * given value obtained from a remove server. This function is
+ * used to set default value for a field in update form (PUT).
+ *
+ * If value type is date, extract only the date portion of the value
+ *
+ * @param schema - schema of given field. Used to determine the default value
+ * @param value - value obtained from remote. Could be undefined or null
+ * @returns value to be set as defaultValue prop for input/select
+ */
+const getDefaultValue = (
+  schema: SchemaElementType,
+  value: string | undefined | null,
+): string => {
+  if (value) {
+    switch (schema.type) {
+      case "date":
+        return value.substring(0, 10);
+      default:
+        return value;
     }
   }
-  return null;
+  return "";
 };
 
-export { capitalise, toSnakeCase, string2Date, removeId };
+/**
+ * Get the string to be display as label for input/select element. If
+ * provided in schema, the schema value will be used. Otherwise, capitalise
+ * the key associated with the schema and return it, or additionally remove
+ * the trailing Id if schema type is select.
+ *
+ * The main use case of this is when label key is different from submission key
+ * (the key param or the name of corresponding input/select). For instance, `Study`
+ * references `Investigation` with `investigation_id` attribute in the backend.
+ * Key or input name is thus set as `investigationId`. If labelKey is not provided,
+ * the label value will be `investigation`
+ *
+ * @param schema - schema correspond to given key.
+ * @param key - key of an entry in Schema
+ * @returns string display value of label
+ */
+const getLabelKey = (schema: SchemaElementType, key: string): string => {
+  if (schema.labelKey) return schema.labelKey;
+  const processedKey =
+    key.length >= 1 ? key[0].toUpperCase() + key.slice(1) : key;
+  return removeId(schema, processedKey);
+};
+
+const getFetcherKey = (schema: SchemaElementType, key: string): string => {
+  if (schema.fetcherKey) return schema.fetcherKey;
+  return removeId(schema, key);
+};
+
+const removeId = (schema: SchemaElementType, key: string): string => {
+  if (schema.type === "select" && key.endsWith("Id"))
+    return key.substring(0, key.length - 2);
+  return key;
+};
+
+const getSubmissionValue = (
+  schema: SchemaElementType,
+  rawValue: FormDataEntryValue,
+): string | Date | File => {
+  if (schema.type === "date" && typeof rawValue === "string")
+    return new Date(rawValue);
+  else return rawValue;
+};
+
+export { getDefaultValue, getLabelKey, getFetcherKey, getSubmissionValue };
--- src/components/types.ts
+++ src/components/types.ts
@@ -1,35 +1,33 @@
-interface AbstractDataType {
-  id?: string;
-  title: string;
-  [key: string]: string | number | Date | null | undefined;
-}
-
 interface SchemaElementType {
-  type: string;
+  /** Type of form entry element */
+  type: "text" | "select" | "date";
+  /** Whether users are required to fill form value */
   required: boolean;
+  /** Place holder value or guide */
   placeholder?: string;
+  /** Key to be used for fetcher - for id related key only */
+  fetcherKey?: string;
+  /** Key to be displayed as label for form data */
+  labelKey?: string;
 }
 
-type AbstractFormDataType<T extends AbstractDataType> = {
-  [K in keyof T]: string;
+interface SchemaType {
+  id: SchemaElementType;
+  title: SchemaElementType;
+  description: SchemaElementType;
+  [k: string]: SchemaElementType;
+}
+
+type DataType<T extends SchemaType> = {
+  [k in keyof T]: string;
 };
 
-type AbstractSchemaType<T extends AbstractDataType> = {
-  [K in keyof T]: SchemaElementType;
-};
-
-interface HandlerType<T extends AbstractDataType> {
-  getAllData: (title?: string | null) => Promise<Array<T>>;
-  getDataById: (id: string) => Promise<T>;
-  createData: (data: T) => Promise<T>;
-  updateData: (data: T, id: string) => Promise<T>;
+interface HandlerType<T extends SchemaType> {
+  getAllData: (title?: string | null) => Promise<Array<DataType<T> | null>>;
+  getDataById: (id: string) => Promise<DataType<T> | null>;
+  createData: (data: T) => Promise<DataType<T>>;
+  updateData: (data: T, id: string) => Promise<DataType<T>>;
   deleteData: (id: string) => Promise<Response>;
 }
 
-export type {
-  AbstractDataType,
-  AbstractFormDataType,
-  SchemaElementType,
-  AbstractSchemaType,
-  HandlerType,
-};
+export type { DataType, SchemaElementType, SchemaType, HandlerType };
