02c6a1018e518806cf49d1f310d18e997c198514
hoangson <lehoangsonsg@gmail.com>
2024-05-13 22:33:08 +0930

    feat: add page component #27

---
+++ src/components/page/page.tsx
@@ -0,0 +1,102 @@
+import { Handler } from "../../handlers";
+import { createInputArray } from "../form";
+import { FetchDataArrayType, FetchDataType, SchemaType } from "../types";
+import { styled } from "@ailiyah-ui/factory";
+import React from "react";
+import { FormComponent } from "../form";
+import { useLoaderData, Link as _Link, Form as _Form } from "react-router-dom";
+import * as Table from "../table";
+
+const Link = styled(_Link);
+const Form = styled(_Form);
+
+function DetailPage({ schema, title }: { schema: SchemaType; title: string }) {
+  const fieldData = useLoaderData() as FetchDataArrayType<SchemaType>;
+  const fields = Object.keys(schema);
+  return (
+    <styled.div themeName="PageRoot">
+      <styled.h1 themeName="PageTitle">{title}</styled.h1>
+      <Form id="search-form" role="search" themeName="PageComponent">
+        <styled.div themeName="PageComponentContent">
+          <styled.input
+            id="title"
+            aria-label="Search title"
+            placeholder="Search"
+            type="search"
+            name="title"
+            themeName="PageSearchInput"
+          />
+          <Link to={`/${title}/create`} themeName="PageNewButton">
+            New Item
+          </Link>
+        </styled.div>
+      </Form>
+      <Table.Root>
+        <Table.Header fields={fields} />
+        <Table.Body fieldData={fieldData} fields={fields} />
+      </Table.Root>
+    </styled.div>
+  );
+}
+
+function CreatePage({
+  children,
+  title,
+}: {
+  children: React.ReactElement[];
+  title: string;
+}) {
+  return (
+    <styled.div themeName="PageRoot">
+      <styled.h1 themeName="PageTitle">{title}</styled.h1>
+      <styled.p themeName="PageDescription">
+        Create a new {title} entry by filling out the form below.
+      </styled.p>
+      <FormComponent method="POST">{children}</FormComponent>;
+    </styled.div>
+  );
+}
+
+function UpdatePage({ schema, title }: { schema: SchemaType; title: string }) {
+  const data = useLoaderData() as FetchDataType<typeof schema>;
+  const updateComponents = React.useMemo(
+    () => createInputArray(schema, [], data),
+    [JSON.stringify(data)],
+  );
+  return (
+    <styled.div themeName="PageRoot">
+      <styled.h1 themeName="PageTitle">{title}</styled.h1>
+      <styled.p themeName="PageDescription">
+        Update the current {title} entry by filling out the form below.
+      </styled.p>
+      <FormComponent method="PUT">{updateComponents}</FormComponent>;
+    </styled.div>
+  );
+}
+
+class Page<T extends SchemaType, Key extends string> {
+  schema: T;
+  handler: Handler<T, Key>;
+  title: string;
+
+  constructor(title: string, schema: T, handler: Handler<T, Key>) {
+    this.schema = schema;
+    this.handler = handler;
+    this.title = title;
+  }
+
+  getCreatePage() {
+    const components = createInputArray(this.schema);
+    return <CreatePage title={this.title}>{components}</CreatePage>;
+  }
+
+  getUpdatePage() {
+    return <UpdatePage title={this.title} schema={this.schema} />;
+  }
+
+  getDetailsPage() {
+    return <DetailPage title={this.title} schema={this.schema} />;
+  }
+}
+
+export { Page };
