e5b6b27c2d3b6c754e93900252fc973f560d3bc7
hoangson <lehoangsonsg@gmail.com>
2024-05-21 15:47:49 +0930

    feat: allow submit to show form data for convenience

--- src/components/select/select.helpers.tsx
+++ src/components/select/select.helpers.tsx
@@ -1,37 +1,58 @@
-import { FetchDataArrayType } from "../types";
+import { FetchDataArrayType, SchemaElementType } from "../types";
 import React from "react";
 import * as Select from "./select";
+import { BaseSchema, parseFormData } from "../helpers";
 
 const fetchData: FetchDataArrayType = [
   { id: "facility0", title: "firstFacility", description: "first facility" },
   { id: "facility1", title: "secondFacility", description: "second facility" },
   { id: "facility2", title: "thirdFacility", description: "third facility" },
   { id: "facility3", title: "fourthFacility", description: "fourth facility" },
+  { id: "facility4", title: "fifthFacility", description: "fifth facility" },
 ];
 
 interface SelectTestProps {
-  name: string;
   required: boolean;
   onSubmit?: (e: React.FormEvent) => void;
   fetchedData?: FetchDataArrayType;
   multiple?: boolean;
   defaultValue?: string | string[];
+  excludeId?: string;
 }
 
 const TestSelectComponent: React.FC<SelectTestProps> = ({
-  onSubmit,
   fetchedData,
   defaultValue,
   multiple,
-  name,
   required,
+  onSubmit,
+  excludeId,
 }) => {
+  const name = "facility";
+  class TestSchema extends BaseSchema {
+    facility: SchemaElementType = {
+      type: "select",
+      required: true,
+      multiple: multiple,
+    };
+  }
+
+  const schema = new TestSchema();
+  const submit = onSubmit
+    ? onSubmit
+    : (e: React.FormEvent) => {
+        e.preventDefault();
+        alert("SubmitForm");
+        const formData = new FormData(e.currentTarget as HTMLFormElement);
+        console.log(parseFormData(schema, formData));
+      };
+
   const defaultValueMap = fetchedData?.reduce((acc, dataItem) => {
     acc.set(dataItem.id as string, dataItem.title as string);
     return acc;
   }, new Map<string, string>());
   return (
-    <form onSubmit={onSubmit} className="flex gap-x-4 w-1/2">
+    <form onSubmit={submit} className="flex gap-x-4 w-1/2">
       <Select.Root
         name={name}
         required={required}
@@ -39,6 +60,7 @@ const TestSelectComponent: React.FC<SelectTestProps> = ({
         placeholder="Select from dropdown"
         defaultValue={defaultValue}
         defaultValueMap={defaultValueMap}
+        excludeId={excludeId}
         themeName="SelectRoot"
       >
         <Select.Trigger themeName="SelectTrigger">
