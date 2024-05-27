4067ca3e63d3820aca31a552d0fa71e9e841a561
hoangson <lehoangsonsg@gmail.com>
2024-05-27 09:52:10 +0930

    feat: update variable - WIP

--- src/routes/variable.ts
+++ src/routes/variable.ts
@@ -3,20 +3,15 @@ import { BaseSchema, SchemaElementType } from "../components";
 class VariableSchema extends BaseSchema {
   timeInterval: SchemaElementType = { type: "text" };
   sampleInterval: SchemaElementType = { type: "text" };
-  deviceTypeId: SchemaElementType = {
+  deviceId: SchemaElementType = {
     type: "select",
     labelKey: "device",
     fetcherKey: "device",
   };
-  unitTypeId: SchemaElementType = {
+  unitId: SchemaElementType = {
     type: "select",
     labelKey: "unit",
     fetcherKey: "unit",
-  };
-  variableTypeId: SchemaElementType = {
-    type: "select",
-    labelKey: "variableType",
-    fetcherKey: "vocabulary",
   };
 }
 
