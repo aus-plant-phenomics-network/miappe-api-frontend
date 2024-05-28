4c86b4ba71b7ad55fc8e3d1c31bf056bb9921a3c
hoangson <lehoangsonsg@gmail.com>
2024-05-29 09:04:32 +0930

    feat: update unit schema and display columns

--- src/routes/unit/index.ts
+++ src/routes/unit/index.ts
@@ -1,6 +1,6 @@
 import { UnitSchema } from "./type";
 import { createRoutes } from "../../factory";
 
-const UnitRoutes = createRoutes(UnitSchema, "unit");
+const UnitRoutes = createRoutes(UnitSchema, "unit", ["name", "symbol"]);
 
 export { UnitRoutes };
