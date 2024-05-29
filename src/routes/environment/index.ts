cd55c9aefa0ddbfceda68da5ae1fad4a9c4f26a3
harryle <lehoangsonsg@gmail.com>
2024-05-29 14:02:55 +0930

    feat: add default environment columns

--- src/routes/environment/index.ts
+++ src/routes/environment/index.ts
@@ -1,6 +1,10 @@
 import { EnvironmentSchema } from "./type";
 import { createRoutes } from "../../factory";
 
-const EnvironmentRoutes = createRoutes(EnvironmentSchema, "environment");
+const EnvironmentRoutes = createRoutes(EnvironmentSchema, "environment", [
+  "parameter",
+  "description",
+  "setpoint",
+]);
 
 export { EnvironmentRoutes };
