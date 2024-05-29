cab55a91bf16892f6fa0cfc87d691d5491e2127e
harryle <lehoangsonsg@gmail.com>
2024-05-29 17:16:22 +0930

    feat: update experimental factor schema

--- src/routes/experimentalFactor/type.ts
+++ src/routes/experimentalFactor/type.ts
@@ -2,7 +2,13 @@ import { SchemaElementType } from "../../components";
 import { VariableSchema } from "../variable";
 
 class ExperimentalFactorType extends VariableSchema {
+  factorDescription: SchemaElementType = { type: "text" };
   factorValues: SchemaElementType = { type: "text" };
+  factorTypeId: SchemaElementType = {
+    type: "select",
+    fetcherKey: "vocabulary",
+    labelKey: "factorType",
+  };
 }
 
 const ExperimentalFactorSchema = new ExperimentalFactorType();
