45a44b3ae16ce735abfe9f8d6c26d838c15d7423
hoangson <lehoangsonsg@gmail.com>
2024-05-30 21:26:19 +0930

    feat: update experiment schema

--- src/routes/experiment/index.ts
+++ src/routes/experiment/index.ts
@@ -1,6 +1,9 @@
 import { ExperimentSchema } from "./type";
 import { createRoutes } from "../../factory";
 
-const ExperimentRoutes = createRoutes(ExperimentSchema, "experiment");
+const ExperimentRoutes = createRoutes(ExperimentSchema, "experiment", [
+  "title",
+  "objective",
+]);
 
 export { ExperimentRoutes };
