b3e524650af1ee6b4ad03342a4acf1ec3f03aa28
hoangson <lehoangsonsg@gmail.com>
2024-06-03 10:32:51 +0930

    feat: update event schema

--- src/routes/event/type.ts
+++ src/routes/event/type.ts
@@ -1,20 +1,27 @@
 import { SchemaElementType, BaseSchema } from "../../components";
 
 class EventType extends BaseSchema {
-  observationUnitId: SchemaElementType = {
-    type: "select",
-    fetcherKey: "observationUnit",
-    labelKey: "observationUnit",
-  };
+  title: SchemaElementType = { type: "text", required: true };
+  description: SchemaElementType = { type: "text" };
+  eventDate: SchemaElementType = { type: "date", required: true };
   eventTypeId: SchemaElementType = {
     type: "select",
     required: true,
     labelKey: "eventType",
     fetcherKey: "vocabulary",
   };
-  date: SchemaElementType = {
-    type: "date",
-    labelKey: "eventDate",
+  observationUnitId: SchemaElementType = {
+    type: "select",
+    fetcherKey: "observationUnit",
+    labelKey: "observationUnit",
+    multiple: true,
+  };
+  studyId: SchemaElementType = {
+    type: "select",
+    multiple: true,
+    required: true,
+    labelKey: "study",
+    fetcherKey: "study",
   };
 }
 
