c324bda684a9508de80b372864ba18c815af8a0e
harryle <lehoangsonsg@gmail.com>
2024-06-17 11:26:01 +0930

    feat: update App.tsx to use raw css

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Root from "./routes/root";
 import { Outlet } from "react-router-dom";
+import "./App.css";
 
 export default function App() {
   return (
-    <div className="flex h-screen">
-      <div className="flex-shrink-0">
+    <div className="App">
+      <div className="AppRoot">
         <Root />
       </div>
-      <div className="mx-10 my-5 flex-grow overflow-y-auto">
+      <div className="AppOutlet">
         <Outlet />
       </div>
     </div>
